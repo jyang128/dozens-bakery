c23a602a0c74093dc20c44a32d8236f613084bfa
jyang128 <jessicayang128@gmail.com>
2019-08-15 22:48:23 +0000

    revised disclaimer handler to use class and setstate

--- client/components/products/product-list.jsx
+++ client/components/products/product-list.jsx
@@ -3,9 +3,18 @@ import ProductListItem from './product-list-item';
 import Disclaimer from '../general/disclaimer';
 
 export default class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      disclaimerClass: ''
+    };
+    this.handleDisclaimer = this.handleDisclaimer.bind(this);
+  }
   handleDisclaimer() {
     sessionStorage.disclaimer = 'hidden';
-    document.querySelector('.disclaimer').classList = ' d-none';
+    this.setState({
+      disclaimerClass: 'd-none'
+    });
   }
   render() {
     const allProducts = this.props.products.map(product =>
@@ -19,7 +28,12 @@ export default class ProductList extends React.Component {
     return (
       <div className="col-12 d-flex justify-content-around flex-wrap">
         {allProducts}
-        {!sessionStorage.disclaimer && <Disclaimer closeDisclaimer={this.handleDisclaimer}/>}
+        {!sessionStorage.disclaimer &&
+          <Disclaimer
+            closeDisclaimer={this.handleDisclaimer}
+            disclaimerClass={this.disclaimerClass}
+          />
+        }
       </div>
     );
   }
